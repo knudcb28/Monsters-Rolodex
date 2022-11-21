e34779eab98740793f0a187318676a0f53079e27
Chris Knudsvig <knudcb28@gmail.com>
2022-11-21 07:54:46 -0600

    minor changes

--- src/App.js
+++ src/App.js
@@ -3,24 +3,37 @@ import { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
+
 class App extends Component {
   constructor() {
     super();
 
     this.state = {
-      name: 'Chris'      
-    }
+      name: {firstName: 'Chris', lastName: 'Knudsvig'},
+      company: 'ZTM',
+    };
   }
+
   render() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         Hi { this.state.name }
+         Hi { this.state.name.firstName } { this.state.name.lastName }, I work at {this.state.company}
         </p>
        <button onClick={() => {
-        this.setState({ name: 'Ellen' })
+        this.setState(
+          () => {
+            return {
+              name: { firstName: 'Ellen', lastName: 'Clark' },
+            };
+          },
+          () => {
+            console.log(this.state);
+          }
+        );
        }}
        >
        Change Name
